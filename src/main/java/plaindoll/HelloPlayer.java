c7f888a34824d4382c6a46d1a9fd3f2902398587
Dogafas <solomindavid10@gmail.com>
2026-05-20 10:21:47 +0700

    Add reply method and test

--- src/main/java/plaindoll/HelloPlayer.java
+++ src/main/java/plaindoll/HelloPlayer.java
@@ -1,9 +1,10 @@
 package plaindoll;
 
 public class HelloPlayer{
-	public static void main(String[] args) {
-		Welcomer welcomer = new Welcomer();
-		System.out.println(welcomer.sayWelcome());
-		System.out.println(welcomer.sayFarewell());
-	}
+    public static void main(String[] args) {
+        Welcomer welcomer = new Welcomer();
+        System.out.println(welcomer.sayWelcome());
+        System.out.println(welcomer.sayFarewell());
+        System.out.println(welcomer.reply("Cody"));
+    }
 }
